51f8ecf74a934ac1d42d936ac44ad5a61c5c7f10
HyeonJeong4031 <hjn40300@gmail.com>
2023-01-31 02:16:25 +0900

    fix - types -> enum 수정

--- back/src/utils/Types.ts
+++ back/src/utils/Types.ts
@@ -1,10 +1,23 @@
-export type emotion =
-  | "신이 난"
-  | "편안한"
-  | "감사한"
-  | "자신감"
-  | "불안"
-  | "슬픔"
-  | "분노"
-  | "상처";
-export type Scope = "all" | "friend" | "off";
+export enum emotion {
+  "신이 난",
+  "편안한",
+  "감사한",
+  "자신감",
+  "불안",
+  "슬픔",
+  "분노",
+  "상처",
+}
+
+export enum Scope {
+  "all",
+  "friend",
+  "off",
+}
+
+export enum friendStatus {
+  Waiting,
+  Accepted,
+  Rejected,
+  Cancellation,
+}

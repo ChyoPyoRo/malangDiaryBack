197697b2dd5f274d07b122839a27cd6efdcd7a23
HyeonJeong4031 <hjn40300@gmail.com>
2023-02-02 18:50:52 +0900

    Feat - QnA 기능 구현

---
+++ back/src/QnA/QnARepository.ts
@@ -0,0 +1,123 @@
+import { user, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import { Inject, Service } from "typedi";
+import {
+  diaryQuestion,
+  diaryAnswer,
+  pagenation,
+} from "./interface/QnAinterface";
+import {
+  EventDispatcher,
+  EventDispatcherInterface,
+} from "../decorators/eventDispatcher";
+
+@Service()
+export class QnARepository {
+  constructor(
+    // @Inject("DiaryQuestion") private question: DiaryQuestion,
+    // @Inject("DiaryAnswer") private AnswerDto: DiaryAnswer,
+    // @Inject("logger") private logger: Logger,
+    @EventDispatcher() private eventDispatcher: EventDispatcherInterface
+  ) {}
+
+  public async addQuestion({
+    question,
+  }: Pick<diaryQuestion, "question">): Promise<diaryQuestion> {
+    const saveData = await prisma.diaryQuestion.create({
+      data: {
+        question: question,
+      },
+    });
+    return saveData;
+  }
+
+  public async getOneQuestion(id: number): Promise<diaryQuestion | null> {
+    const getOneQuestion = await prisma.diaryQuestion.findFirst({
+      where: { id: id },
+    });
+    return getOneQuestion;
+  }
+
+  public async postAnswerQnA(
+    QnA: Pick<diaryAnswer, "questionNumber" | "userId" | "content">
+  ): Promise<diaryAnswer | null> {
+    const postAnswer = await prisma.diaryAnswer.create({
+      data: {
+        content: QnA.content,
+        user: {
+          connect: { id: QnA.userId },
+        },
+        diaryQuestion: {
+          connect: {
+            id: QnA.questionNumber,
+          },
+        },
+      },
+    });
+    return postAnswer;
+  }
+
+  // web
+  public async getWebAnswersList(
+    data: Pick<diaryAnswer, "userId"> & pagenation
+  ): Promise<
+    (diaryAnswer & {
+      user: user;
+    } & { diaryQuestion: diaryQuestion })[]
+  > {
+    const answerList = await prisma.diaryAnswer.findMany({
+      where: { userId: data.userId },
+      include: { user: true, diaryQuestion: true },
+      orderBy: {
+        createAt: "desc",
+      },
+      skip: (data.page - 1) * 5,
+      take: 5,
+    });
+    return answerList;
+  }
+
+  // app
+  public async getAppAnswersList(
+    data: Pick<diaryAnswer, "userId"> & pagenation
+  ): Promise<
+    (diaryAnswer & {
+      user: user;
+    } & { diaryQuestion: diaryQuestion })[]
+  > {
+    const answerList = await prisma.diaryAnswer.findMany({
+      where: {
+        userId: data.userId,
+        createAt: {
+          gte: data.startDate,
+          lte: data.endDate,
+        },
+      },
+      include: { user: true, diaryQuestion: true },
+      orderBy: {
+        createAt: "desc",
+      },
+    });
+
+    return answerList;
+  }
+
+  public async getDate(userId: number) {
+    const latestAnswer = await prisma.diaryAnswer.findFirst({
+      where: { userId },
+      orderBy: {
+        createAt: "desc",
+      },
+      include: {
+        diaryQuestion: true,
+      },
+    });
+    return latestAnswer;
+  }
+  public async CountOfQuestion(userId: number) {
+    const questionInfo = await prisma.diaryAnswer.count({
+      where: { userId },
+    });
+    return questionInfo;
+  }
+}
---
+++ back/src/QnA/QnAService.ts
@@ -0,0 +1,86 @@
+import {
+  diaryQuestion,
+  diaryAnswer,
+  pagenation,
+  page,
+} from "./interface/QnAinterface";
+import { DiaryQuestion, DiaryAnswer, user } from "@prisma/client";
+import { Inject, Service, Container } from "typedi";
+import "reflect-metadata";
+import {
+  EventDispatcher,
+  EventDispatcherInterface,
+} from "../decorators/eventDispatcher";
+import { QnARepository } from "./QnARepository";
+import { number } from "joi";
+
+@Service()
+export default class QnAService {
+  public dateTime: Date;
+  constructor(
+    public questionId: number = 0,
+    @EventDispatcher() private eventDispatcher: EventDispatcherInterface
+  ) {
+    this.dateTime = new Date();
+  }
+
+  public async postQuestion(question: Pick<diaryQuestion, "question">) {
+    const QnARepositoryInstance = Container.get(QnARepository);
+    const savedData = await QnARepositoryInstance.addQuestion(question);
+
+    return savedData;
+  }
+  public async getQuestion(userId: number): Promise<diaryQuestion | null> {
+    const QnARepositoryInstance = Container.get(QnARepository);
+    // new instance  ==== get count 관련
+    const count: number = await QnARepositoryInstance.CountOfQuestion(userId);
+
+    //  new instance  ==== get Date 관련
+    const answerRecord = await QnARepositoryInstance.getDate(userId);
+
+    if (!answerRecord) {
+      //  기존 작성 대답 없으면 1번부터
+      this.questionId = 1;
+    } else if (answerRecord.createAt.getDate() < this.dateTime.getDate()) {
+      //  최근 답변의 질문번호 +1을 리턴 혹은 최대 질문 수 오버시 1로 초기화
+      answerRecord.diaryQuestion.id >= count
+        ? (this.questionId = 1)
+        : (this.questionId = answerRecord.diaryQuestion.id + 1);
+    } else {
+      // 최근 답변 일시 == 오늘
+      this.questionId = answerRecord.diaryQuestion.id;
+    }
+
+    const getOneQuestion = await QnARepositoryInstance.getOneQuestion(
+      this.questionId
+    );
+    return getOneQuestion;
+  }
+
+  public async postAnswer(
+    QnA: Pick<diaryAnswer, "questionNumber" | "userId" | "content">
+  ): Promise<diaryAnswer | null> {
+    const QnARepositoryInstance = Container.get(QnARepository);
+    const postAnswer = await QnARepositoryInstance.postAnswerQnA(QnA);
+    return postAnswer;
+  }
+
+  public async getAnswer(
+    data: Pick<diaryAnswer, "userId"> & pagenation
+  ): Promise<
+    (diaryAnswer & {
+      user: user;
+    } & { diaryQuestion: diaryQuestion })[]
+  > {
+    let getAnswersList;
+    const QnARepositoryInstance = Container.get(QnARepository);
+    if (Number.isNaN(data.page)) {
+      return (getAnswersList = await QnARepositoryInstance.getAppAnswersList(
+        data
+      ));
+    } else {
+      getAnswersList = await QnARepositoryInstance.getWebAnswersList(data);
+    }
+    return getAnswersList;
+  }
+}
